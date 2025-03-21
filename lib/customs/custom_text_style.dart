04ff340a68108937f549feb44a27601d36274cae
youssef frahat <108248554+youssef-frahat@users.noreply.github.com>
2025-03-22 00:43:12 +0200

    making text style

---
+++ lib/customs/custom_text_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+TextStyle mainText = TextStyle(
+  fontFamily: 'Changa', // Use your desired font family
+  fontSize: 14, // Adjust the font size
+  fontWeight: FontWeight.bold, // Adjust the font weight
+  color: Color(0xFFF8A435), // Adjust the text color
+);
+
+TextStyle prices = TextStyle(
+  fontFamily: 'Changa', // Use your desired font family
+  fontSize: 12, // Adjust the font size
+  color: Color(0xFF000000), // Adjust the text color
+);
+
