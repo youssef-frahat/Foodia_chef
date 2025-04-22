43cb6efb0b8836c21cc14fb6d7424ee0c5ca74b9
youssef frahat <108248554+youssef-frahat@users.noreply.github.com>
2025-04-22 23:28:39 +0200

    feat: Integrate shared preferences for token management; update login flow and app initialization

--- lib/fodiaa.dart
+++ lib/fodiaa.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodia_chef/core/helpers/shared_pref_local_storage.dart';
+import 'package:foodia_chef/core/routes/routes.dart';
 
 import 'core/routes/app_router.dart';
 
@@ -14,16 +16,30 @@ class Foodia extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(428, 922),
       splitScreenMode: true,
-
       minTextAdapt: true,
       child: MaterialApp.router(
-          localizationsDelegates: context.localizationDelegates,
+        localizationsDelegates: context.localizationDelegates,
         supportedLocales: context.supportedLocales,
         locale: context.locale,
-        routerConfig: appRouter.router, 
+        routerConfig: appRouter.router,
         title: 'Foodia Chef',
         debugShowCheckedModeBanner: false,
       ),
+    );
+  }
+}
+
+checkLogin() async {
+  final token = SharedPref.sharedPreferences.getString("token");
+  if (token != null) {
+    return AppRouter.appNavigatorKey.currentState?.pushNamedAndRemoveUntil(
+      Routes.home,
+      (route) => false,
+    );
+  } else {
+    return AppRouter.appNavigatorKey.currentState?.pushNamedAndRemoveUntil(
+      Routes.login,
+      (route) => false,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodia_chef/core/helpers/bloc_observer.dart';
+import 'package:foodia_chef/core/helpers/shared_pref_local_storage.dart';
 import 'package:foodia_chef/fodiaa.dart';
 
 import 'core/di/dependency_injection.dart';
@@ -12,6 +13,7 @@ import 'core/routes/app_router.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await SharedPref.init();
 
   Bloc.observer = MyBlocObserver();
   setupGetIt();
